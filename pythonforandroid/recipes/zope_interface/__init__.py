6aecfca757d11fe4d46d96efe2c218546e232a0d
Richard Tibbles <richard@learningequality.org>
2021-09-18 14:45:48 -0700

    Remove patch from zope_interface that no longer seems to be needed.

--- pythonforandroid/recipes/zope_interface/__init__.py
+++ pythonforandroid/recipes/zope_interface/__init__.py
@@ -1,6 +1,5 @@
 from pythonforandroid.recipe import PythonRecipe
 from pythonforandroid.toolchain import current_directory
-from os.path import join
 import sh
 
 
@@ -12,15 +11,6 @@ class ZopeInterfaceRecipe(PythonRecipe):
     site_packages_name = 'zope.interface'
     depends = ['setuptools']
     patches = ['no_tests.patch']
-
-    def build_arch(self, arch):
-        super().build_arch(arch)
-        # The zope.interface module lacks of the __init__.py file in one of his
-        # folders (once is installed), that leads into an ImportError.
-        # Here we intentionally apply a patch to solve that, so, in case that
-        # this is solved in the future an error will be triggered
-        zope_install = join(self.ctx.get_site_packages_dir(arch.arch), 'zope')
-        self.apply_patch('fix-init.patch', arch.arch, build_dir=zope_install)
 
     def prebuild_arch(self, arch):
         super().prebuild_arch(arch)
